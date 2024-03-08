f80e98a42d44ef8d15d40106fa8551ec0206f13b
Dhruv Parmar <dhruvparmar051@gmail.com>
2024-03-09 00:52:18 +0530

    user Details with access control

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,22 +1,24 @@
-import AgencyDetails from "@/components/forms/agency-details";
-import { db } from "@/lib/db";
-import { currentUser } from "@clerk/nextjs";
-import userDetails from "@/components/forms/user-details";
+import AgencyDetails from '@/components/forms/agency-details'
+import UserDetails from '@/components/forms/user-details'
+import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs'
+import React from 'react'
 
 type Props = {
-  params: { agencyId: string };
-};
+  params: { agencyId: string }
+}
 
 const SettingsPage = async ({ params }: Props) => {
-  const authUser = await currentUser();
-  if (!authUser) return null;
+  const authUser = await currentUser()
+  if (!authUser) return null
 
   const userDetails = await db.user.findUnique({
-    where: { email: authUser.emailAddresses[0].emailAddress },
-  });
+    where: {
+      email: authUser.emailAddresses[0].emailAddress,
+    },
+  })
 
-  if (!userDetails) return null;
-
+  if (!userDetails) return null
   const agencyDetails = await db.agency.findUnique({
     where: {
       id: params.agencyId,
@@ -24,23 +26,23 @@ const SettingsPage = async ({ params }: Props) => {
     include: {
       SubAccount: true,
     },
-  });
+  })
 
-  if (!agencyDetails) return null;
+  if (!agencyDetails) return null
 
-  const subAccount = agencyDetails.SubAccount;
+  const subAccounts = agencyDetails.SubAccount
 
   return (
-    <div className="flex ld:!flex-row flex-col gap-4">
+    <div className="flex lg:!flex-row flex-col gap-4">
       <AgencyDetails data={agencyDetails} />
-      <userDetails
+      <UserDetails
         type="agency"
         id={params.agencyId}
-        subAccount={subAccount}
+        subAccounts={subAccounts}
         userData={userDetails}
       />
     </div>
-  );
-};
+  )
+}
 
-export default SettingsPage;
+export default SettingsPage
